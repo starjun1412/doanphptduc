43215bc54f752a5417e08bee9f4aea4508cad7ab
starjun1412 <lehominhquan94@gmail.com>
2020-06-06 11:34:53 +0700

    Day du lieu len trang chu

--- complete-blog-php/public/headtop.php
+++ complete-blog-php/public/headtop.php
@@ -2,11 +2,11 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-12 col-lg-6 d-flex">
-            <a href="index.html" class="site-logo">
-              Meranda
+            <a href="index.php" class="site-logo text-info">
+              FIGURE BÉO BỰ
             </a>
 
-            <a href="#" class="ml-auto d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+            <a href="#" class="ml-auto d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-danger"><span
                 class="icon-menu h3"></span></a>
 
           </div>
@@ -44,7 +44,7 @@
             <nav class="site-navigation position-relative text-right" role="navigation">
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none pl-0 d-lg-block">
                 <li class="active">
-                  <a href="index.html" class="nav-link text-left">Home</a>
+                  <a href="index.php" class="nav-link text-left">Home</a>
                 </li>
                 <li>
                   <a href="categories.html" class="nav-link text-left">Categories</a>
---
+++ complete-blog-php/public/slideview.php
@@ -0,0 +1,77 @@
+<div class="site-section py-0">
+      <div class="owl-carousel hero-slide owl-style">
+
+        <div class="site-section">
+          <div class="container">
+            <div class="half-post-entry d-block d-lg-flex bg-light">
+              <div class="img-bg" style="background-image: url('images/enel.jpg')"></div>
+              <div class="contents">
+                <span class="caption">Limit 368 bản</span>
+                <h2><a href="blog-single.html">LLittle Apple Studio - Enel</a></h2>
+                <p class="mb-3">
+                   Trọng lượng : 10-12kg <br>
+                   Chất liệu :Resin <br>
+                    Quý 4 / 2020  <br>
+                    Limit 368 bản <br>
+                    Giá : 7tr9 cọc 2tr9</p>
+                
+                <div class="post-meta">
+                  <span class="d-block"><a href="#">Resin Real</a> in <a href="#">Resin</a></span>
+                
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="site-section">
+          <div class="container">
+            <div class="half-post-entry d-block d-lg-flex bg-light">
+              <div class="img-bg" style="background-image: url('images/goku.jpg')"></div>
+              <div class="contents">
+                <span class="caption">Limit 46 bản</span>
+                <h2><a href="blog-single.html">Super Studio - Songoku</a></h2>
+                <p class="mb-3">
+                   Trọng lượng : 14-16kg <br>
+                   Chất liệu :Resin-PVC <br>
+                    Quý 4 / 2020  <br>
+                    Limit 46 bản <br>
+                    Giá : 17tr9 cọc 7tr9</p>
+                
+                <div class="post-meta">
+                  <span class="d-block"><a href="#">Resin+ Real</a> in <a href="#">Resin</a></span>
+                
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="site-section">
+          <div class="container">
+            <div class="half-post-entry d-block d-lg-flex bg-light">
+              <div class="img-bg" style="background-image: url('images/rose_goku.jpg')"></div>
+              <div class="contents">
+                <span class="caption">Limit 35 bản</span>
+                <h2><a href="blog-single.html">LLD Studio - Dragonball Super Zamasu Rose Goku GK </a></h2>
+                <p class="mb-3">
+                   Trọng lượng : 10-12kg <br>
+                   Chất liệu :Resin <br>
+                    Quý 3 / 2020  <br>
+                    Limit 35 bản <br>
+                    Giá : 27tr cọc 30-50%</p>
+                
+                <div class="post-meta">
+                  <span class="d-block"><a href="#">Resin Real</a> in <a href="#">Resin</a></span>
+                
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  
